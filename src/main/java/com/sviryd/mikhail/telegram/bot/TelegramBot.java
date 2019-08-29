a18a8bf01a3ce2f12f9f79c50d326f9d550d44ba
mgsviryd <mgsviryd@gmail.com>
2019-08-29 23:15:56 +0300

    Telegram bot: add template class

--- src/main/java/com/sviryd/mikhail/telegram/bot/TelegramBot.java
+++ src/main/java/com/sviryd/mikhail/telegram/bot/TelegramBot.java
@@ -1,4 +1,27 @@
 package com.sviryd.mikhail.telegram.bot;
 
-public class TelegramBot {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+    private String botUsername;
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
 }
