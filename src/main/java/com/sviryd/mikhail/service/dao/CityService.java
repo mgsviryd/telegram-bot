0f17773a376dbb1e1f74d80c4fddd11453ac9846
mgsviryd <mgsviryd@gmail.com>
2019-08-29 17:46:04 +0300

    Service: create CityService

--- src/main/java/com/sviryd/mikhail/service/dao/CityService.java
+++ src/main/java/com/sviryd/mikhail/service/dao/CityService.java
@@ -1,4 +1,29 @@
 package com.sviryd.mikhail.service.dao;
 
+import com.sviryd.mikhail.dao.entity.City;
+
+import java.util.Optional;
+
 public interface CityService {
+    City save(City var1);
+
+    Iterable<City> saveAll(Iterable<City> var1);
+
+    Optional<City> findById(Long var1);
+
+    boolean existsById(Long var1);
+
+    Iterable<City> findAll();
+
+    Iterable<City> findAllById(Iterable<Long> var1);
+
+    long count();
+
+    void deleteById(Long var1);
+
+    void delete(City var1);
+
+    void deleteAll(Iterable<City> var1);
+
+    void deleteAll();
 }
--- src/main/java/com/sviryd/mikhail/service/dao/impl/CityServiceImpl.java
+++ src/main/java/com/sviryd/mikhail/service/dao/impl/CityServiceImpl.java
@@ -1,12 +1,70 @@
-package com.sviryd.mikhail.service.dao;
+package com.sviryd.mikhail.service.dao.impl;
 
+import com.sviryd.mikhail.dao.entity.City;
 import com.sviryd.mikhail.repos.CityRepo;
+import com.sviryd.mikhail.service.dao.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class CityService extends Service {
+public class CityServiceImpl implements CityService {
     @Autowired
     private CityRepo cityRepo;
 
+    @Override
+    public City save(City city) {
+        return cityRepo.save(city);
+    }
+
+    @Override
+    public Iterable<City> saveAll(Iterable<City> cities) {
+        return cityRepo.saveAll(cities);
+    }
+
+    @Override
+    public Optional<City> findById(Long id) {
+        return cityRepo.findById(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return cityRepo.existsById(id);
+    }
+
+    @Override
+    public Iterable<City> findAll() {
+        return cityRepo.findAll();
+    }
+
+    @Override
+    public Iterable<City> findAllById(Iterable<Long> ids) {
+        return cityRepo.findAllById(ids);
+    }
+
+    @Override
+    public long count() {
+        return cityRepo.count();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        cityRepo.deleteById(id);
+    }
+
+    @Override
+    public void delete(City city) {
+        cityRepo.delete(city);
+    }
+
+    @Override
+    public void deleteAll(Iterable<City> cities) {
+        cityRepo.deleteAll(cities);
+    }
+
+    @Override
+    public void deleteAll() {
+        cityRepo.deleteAll();
+    }
 }
