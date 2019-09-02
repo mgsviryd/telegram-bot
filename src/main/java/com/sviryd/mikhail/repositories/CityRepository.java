6ad46968096e2f1c37667b9b0c615697c2fbb048
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:10:24 +0300

    Repository: expand to jpa ability

--- src/main/java/com/sviryd/mikhail/repositories/CityRepository.java
+++ src/main/java/com/sviryd/mikhail/repositories/CityRepository.java
@@ -1,10 +1,10 @@
 package com.sviryd.mikhail.repositories;
 
 import com.sviryd.mikhail.entity.City;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CityReporitory extends CrudRepository<City, Long> {
+public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> findByName(String name);
 }
