e33f1f2a3d9554ff91a49a5bbf3b8708382a7ca2
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:21:51 +0300

    Validation: validate entity: city

--- src/main/java/com/sviryd/mikhail/entity/City.java
+++ src/main/java/com/sviryd/mikhail/entity/City.java
@@ -2,14 +2,20 @@ package com.sviryd.mikhail.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -17,6 +23,11 @@ public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotBlank(message = "Name cannot be blank.")
+    @Length(max = 255)
     private String name;
+    @NotBlank(message = "Information cannot be blank.")
+    @Length(max = 2048)
     private String information;
 }
