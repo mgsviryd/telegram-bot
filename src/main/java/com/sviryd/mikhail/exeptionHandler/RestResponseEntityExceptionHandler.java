6cce47eed21039d30734d2030e982def4bddf1f3
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:09:12 +0300

    ExceptionHandler: implement as global handler(take type exception - return HttpStatus)

--- src/main/java/com/sviryd/mikhail/exeptionHandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/sviryd/mikhail/exeptionHandler/RestResponseEntityExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.sviryd.mikhail.exeptionHandler;
 
-public class RestResponseEntityExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ValidationException;
+
+@ControllerAdvice
+@RestController
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+    @ExceptionHandler(value = {ValidationException.class})
+    protected ResponseEntity<Object> handleMethodArgumentNotValidException(Exception ex, WebRequest request) {
+        String bodyOfResponse = "Validation Failed";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
 }
