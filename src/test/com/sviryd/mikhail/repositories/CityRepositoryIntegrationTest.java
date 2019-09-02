0467c40491c64a8fe0338a84e70b8e840c4f2424
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:20:39 +0300

    Test: create CityRepositoryIntegrationTest

--- src/test/com/sviryd/mikhail/repositories/CityRepositoryIntegrationTest.java
+++ src/test/com/sviryd/mikhail/repositories/CityRepositoryIntegrationTest.java
@@ -6,21 +6,27 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class CityReporitoryIntegrationTest {
+@TestPropertySource("/application-integrationtest.properties")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class CityRepositoryIntegrationTest {
 
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
-    private CityReporitory cityReporitory;
+    private CityRepository cityRepository;
 
     @Test
     public void whenFindByName_thenReturnCity() {
@@ -30,11 +36,9 @@ public class CityReporitoryIntegrationTest {
         entityManager.persist(city);
         entityManager.flush();
 
-        final Optional<City> cityOptional = cityReporitory.findByName(name);
+        final Optional<City> cityOptional = cityRepository.findByName(name);
         Assert.assertTrue(cityOptional.isPresent());
 
-        Assertions.assertThat(cityOptional.get().getName())
-                .isEqualTo(name);
+        Assertions.assertThat(cityOptional.get().getName()).isEqualTo(name);
     }
-
 }
