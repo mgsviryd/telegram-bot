a8342f0d36e289980ffb2e583212d1e764d98851
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:07:04 +0300

    Flyway: implement migration

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,3 +1,8 @@
+CREATE TABLE hibernate_sequence (
+  next_val BIGINT
+);
+INSERT INTO hibernate_sequence VALUES (1);
+INSERT INTO hibernate_sequence VALUES (1);
 CREATE TABLE city (
   id          BIGINT        NOT NULL,
   name        VARCHAR(255),
