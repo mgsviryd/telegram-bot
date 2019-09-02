e447e6a4a0c8d86701ba960fdb3e4a0a18f8e270
mgsviryd <mgsviryd@gmail.com>
2019-09-02 19:24:43 +0300

    DB schema: City all fields are NOT NULL

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -5,7 +5,7 @@ INSERT INTO hibernate_sequence VALUES (1);
 INSERT INTO hibernate_sequence VALUES (1);
 CREATE TABLE city (
   id          BIGINT        NOT NULL,
-  name        VARCHAR(255),
+  name        VARCHAR(255)  NOT NULL,
   information VARCHAR(2048) NOT NULL,
   PRIMARY KEY (id)
 );
