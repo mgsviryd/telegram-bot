0f809b7fb157395424effe96f9782a7f8816734e
mgsviryd <mgsviryd@gmail.com>
2025-01-23 00:48:00 +0300

    test: add readable comments to CityRestControllerTest

--- src/test/java/com/sviryd/telegram_bot/controller/CityRestControllerTest.java
+++ src/test/java/com/sviryd/telegram_bot/controller/CityRestControllerTest.java
@@ -21,12 +21,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+/**
+ * Тестовый класс для проверки функциональности REST контроллера `CityRestController`.
+ *
+ * Конфигурация:
+ * - Используется `SpringBootTest` с случайным портом.
+ * - `@AutoConfigureMockMvc` для тестирования с помощью MockMvc.
+ * - База данных инициализируется с помощью SQL-скриптов до и после каждого теста.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("it")
 @Sql(value = {"/init-db-city-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/cleanup-db-city-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-public class CityRestControllerIT {
+public class CityRestControllerTest {
     @Autowired
     private MockMvc mvc;
     @Autowired
@@ -51,6 +59,9 @@ public class CityRestControllerIT {
         assertThat(cityRestController).isNotNull();
     }
 
+    // Тест для проверки сохранения города через REST API.
+    // Ожидается, что при успешном сохранении статус ответа будет 201 (Created)
+    // и в ответе будет существовать поле "id".
     @Test
     public void testSave() throws Exception {
         mvc.perform(post("/cities")
@@ -61,6 +72,10 @@ public class CityRestControllerIT {
                 .andExpect(jsonPath("id").exists());
     }
 
+    /**
+     * Тестовый метод, который проверяет, что попытка сохранения города
+     * с нулевым содержимым возвращает статус 400 Bad Request.
+     */
     @Test
     public void testSaveFailNull() throws Exception {
         mvc.perform(post("/cities")
@@ -70,6 +85,10 @@ public class CityRestControllerIT {
                 .andExpect(status().isBadRequest());
     }
 
+    /**
+     * Тест проверяет, что при попытке сохранить город с недопустимой информацией (null)
+     * возвращается статус ошибки 400 Bad Request.
+     */
     @Test
     public void testSaveFailValidation() throws Exception {
         city.setInformation(null);
@@ -80,13 +99,19 @@ public class CityRestControllerIT {
                 .andExpect(status().isBadRequest());
     }
 
+    /**
+     * Тестовый метод для проверки обновления информации о городе.
+     * Он выполняет два запроса PUT для обновления города и проверяет,
+     * что статус ответа - 200 OK, а также что возвращаемые данные содержат
+     * правильные значения идентификатора, имени и информации о городе.
+     * Второй запрос также проверяет, что информация о городе изменилась.
+     */
     @Test
     public void testUpdate() throws Exception {
         City oldCity = new City();
         oldCity.setId(city.getId());
         oldCity.setName(city.getName());
         oldCity.setInformation("Moscow is the biggest city in Russia.");
-
         mvc.perform(put("/cities/{id}", oldCity.getId())
                         .content(new ObjectMapper().writeValueAsString(oldCity))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -106,6 +131,12 @@ public class CityRestControllerIT {
                 .andExpect(MockMvcResultMatchers.jsonPath("information", not(oldCity.getInformation())));
     }
 
+    /**
+     * Тестовый метод для проверки получения города по идентификатору.
+     * Он выполняет GET-запрос к конечной точке "/cities/{id}",
+     * ожидает статус 200 OK и проверяет, что возвращаемый JSON
+     * содержит правильные значения для полей "name" и "information".
+     */
     @Test
     public void testFindById() throws Exception {
         mvc.perform(get("/cities/{id}", city.getId())
@@ -117,6 +148,10 @@ public class CityRestControllerIT {
                 .andExpect(jsonPath("information", is(city.getInformation())));
     }
 
+    /**
+     * Тест проверяет, что при запросе ресурса с несуществующим идентификатором
+     * сервер возвращает HTTP статус 404 (Not Found).
+     */
     @Test
     public void testFindByIdFailNotFound() throws Exception {
         mvc.perform(get("/cities/{id}", Long.MAX_VALUE)
@@ -124,6 +159,9 @@ public class CityRestControllerIT {
                 .andExpect(status().isNotFound());
     }
 
+    /**
+     * Тест проверяет успешное выполнение фильтрации городов по имени.
+     */
     @Test
     public void testFindByName() throws Exception {
         mvc.perform(get("/cities/filtering/byName/{name}", city.getName())
@@ -135,6 +173,9 @@ public class CityRestControllerIT {
                 .andExpect(jsonPath("information", is(city.getInformation())));
     }
 
+    /**
+     * Тест проверяет успешное удаление города по идентификатору.
+     */
     @Test
     public void testDeleteById() throws Exception {
         mvc.perform(delete("/cities/{id}", city.getId()))
